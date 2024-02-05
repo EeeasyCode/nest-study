ab3d75296dc3908fb37d4b38d723056bbf050fb3
Khan Lee <khan.lee@wclub.co.kr>
2024-02-05 15:13:51 +0900

    [FEAT] test 폴더 구조 변경 및 커버리지 작성

---
+++ test/some-test/app.controller.unit.spec.ts
@@ -0,0 +1,34 @@
+import { Test } from '@nestjs/testing';
+import { AppController } from '../../src/app.controller';
+import { AppService } from '../../src/app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
+  });
+
+  describe('hello', () => {
+    it('returns a default message', () => {
+      jest.spyOn(appService, 'getHello').mockReturnValue('Hey~');
+      expect(appController.hello()).toEqual({ message: 'Hey~'});
+      expect(appService.getHello).toHaveBeenCalledTimes(1);
+      expect(appService.getHello).toHaveBeenCalledWith('World');
+    });
+
+    it('returns a personalized message', () => {
+      jest.spyOn(appService, 'getHello').mockReturnValue('Hey, John~');
+      expect(appController.hello('John')).toEqual({ message: 'Hey, John~'});
+      expect(appService.getHello).toHaveBeenCalledTimes(1);
+      expect(appService.getHello).toHaveBeenCalledWith('John');
+    })
+  });
+});
