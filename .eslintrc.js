c85ac81412e00e3785f9413bfafa848d486d80ca
Khan Lee <khan.lee@wclub.co.kr>
2024-02-16 16:06:45 +0900

    [TEST] convention test

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'no-var': 'error',
-    'no-unused-var': 'off',
   },
 };
