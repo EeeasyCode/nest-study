7553a3fa68803a7eaa52b7563205c96c755de53f
Khan Lee <khan.lee@wclub.co.kr>
2024-02-20 12:13:46 +0900

    [FEAT] typeorm 설정 및 users 모듈과 연동

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
+import importModules from './modules';
 
 @Module({
-  imports: [UsersModule],
+  imports: [...importModules],
   controllers: [],
   providers: []
 })
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { Users } from './entities/users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
