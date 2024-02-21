0ba3755d4b5191722e682564bcc0088d2eaf0a1c
Khan Lee <khan.lee@wclub.co.kr>
2024-02-21 11:31:58 +0900

    [FEAT] entity 타입 설정

--- src/core/decorators/custom.repository.decorator.ts
+++ src/core/decorators/custom.repository.decorator.ts
@@ -1,7 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 
 export const CUSTOM_REPOSITORY = 'CUSTOM_REPOSITORY';
 
-export function CustomRepository(entity): ClassDecorator {
+export function CustomRepository(entity: EntityClassOrSchema): ClassDecorator {
   return SetMetadata(CUSTOM_REPOSITORY, entity);
 }
