49e829e03ee9ff40cc80f31bc1c6016a4667855d
test <test@gmail.com>
2024-05-18 21:55:33 +0900

    feat: main branch 정리

--- src/core/decorators/custom.repository.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
-
-export const CUSTOM_REPOSITORY = 'CUSTOM_REPOSITORY';
-
-export function CustomRepository(entity: EntityClassOrSchema): ClassDecorator {
-  return SetMetadata(CUSTOM_REPOSITORY, entity);
-}
