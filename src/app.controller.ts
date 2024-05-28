b834ec80acf0512da592b40f66f62e4dccf8f6e7
test <test@gmail.com>
2024-05-28 14:10:04 +0900

    feat: file-interceptor 코드 분석

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseInterceptors(FileInterceptor('file'))
   @Get()
   getHello(): string {
     return this.appService.getHello();
